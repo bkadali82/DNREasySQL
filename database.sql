7be63386915d4e4a4fcec9266f810aab7fa7449d
Kadali, Bhanu <Bhanu.Kadali@capitalone.com>
2015-07-18 07:19:33 -0500

    Update to table

--- database.sql
+++ database.sql
@@ -8,31 +8,33 @@ USE `CPUPLOAD`;
 
 DROP TABLE IF EXISTS `CouponData`;
 CREATE TABLE `CouponData` (
-  `CouponID` varchar(16) NOT NULL,
+  `CouponID` varchar(16) NOT NULL AUTO_INCREMENT,
   `Merchant` varchar(16) DEFAULT NULL,
-  `BussinessType` varchar(11) DEFAULT NULL,
+  `BussinessType` varchar(20) DEFAULT NULL,
   `Address` varchar(256) DEFAULT NULL,
   `zipcode` varchar(16) DEFAULT NULL,
   `longitude` varchar(16) DEFAULT NULL,
   `lat` varchar(16) DEFAULT NULL,
-  'couponimage' blob default null, 
+  `couponimage` blob DEFAULT NULL, 
   PRIMARY KEY (`CouponID`)
 );
 
 
 DROP TABLE IF EXISTS `Customer`;
 CREATE TABLE `Customer` (
-  `CustomerEmail` varchar(16) NOT NULL,
+  `CustomerEmail` varchar(100) NOT NULL,
   `CustomerName` varchar(50) DEFAULT NULL,
   `Address` varchar(256) DEFAULT NULL,
   `zipcode` varchar(16) DEFAULT NULL,
   `phonenumber` varchar(10) DEFAULT NULL,
+  `directReferal1` varchar(100) DEFAULT NULL,
+  `directReferal2` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`CustomerEmail`)
 );
 
 
 LOCK TABLES `CouponData` WRITE;
 INSERT INTO `CouponData` VALUES
-  ('1122233','TGIF','Restuarent','123 address','60008','42.050123','-88.042236'),
-  ('1122234','CPK','Restuarent','123 address','60008','42.051051	','-88.040167');
+  ('1122233','TGIF','Restuarent','123 address','60008','42.050123','-88.042236',null),
+  ('1122234','CPK','Restuarent','123 address','60008','42.051051	','-88.040167',null);
 UNLOCK TABLES;
