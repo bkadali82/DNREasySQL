655dcb748afca4d4cbce1a18262ff5777a82cdfc
Kadali, Bhanu <Bhanu.Kadali@capitalone.com>
2015-07-17 20:49:53 -0500

    update to database

--- database.sql
+++ database.sql
@@ -13,28 +13,26 @@ CREATE TABLE `CouponData` (
   `BussinessType` varchar(11) DEFAULT NULL,
   `Address` varchar(256) DEFAULT NULL,
   `zipcode` varchar(16) DEFAULT NULL,
-  `longitutde` varchar(16) DEFAULT NULL,
+  `longitude` varchar(16) DEFAULT NULL,
   `lat` varchar(16) DEFAULT NULL,
+  'couponimage' blob default null, 
   PRIMARY KEY (`CouponID`)
 );
 
---
--- Table structure for table `Studiengang`
---
---
---DROP TABLE IF EXISTS `Studiengang`;
---CREATE TABLE `Studiengang` (
---  `SGID` varchar(16) NOT NULL,
---  `Bezeichnung` varchar(128) DEFAULT NULL,
---  `Abschluss` varchar(16) DEFAULT NULL,
---  PRIMARY KEY (`SGID`)
---);
---
--- Dumping data for table `Lehrveranstaltung`
---
+
+DROP TABLE IF EXISTS `Customer`;
+CREATE TABLE `Customer` (
+  `CustomerEmail` varchar(16) NOT NULL,
+  `CustomerName` varchar(50) DEFAULT NULL,
+  `Address` varchar(256) DEFAULT NULL,
+  `zipcode` varchar(16) DEFAULT NULL,
+  `phonenumber` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`CustomerEmail`)
+);
+
 
 LOCK TABLES `CouponData` WRITE;
 INSERT INTO `CouponData` VALUES
   ('1122233','TGIF','Restuarent','123 address','60008','42.050123','-88.042236'),
-  ('1122234','CPK','Restuarent','123 address','60008','42.051051','-88.040167');
+  ('1122234','CPK','Restuarent','123 address','60008','42.051051	','-88.040167');
 UNLOCK TABLES;
